9c8d0e40ac9851b8ce8e49ae77d442de9a6f9377
oncorphinchus <tri.nguyen@bbzbl-it.ch>
2025-02-26 14:03:05 +0100

    vercel edit aws

--- app/lib/aws-config.ts
+++ app/lib/aws-config.ts
@@ -2,6 +2,10 @@ import { S3Client } from '@aws-sdk/client-s3'
 
 export const BUCKET_NAME = process.env.BUCKET_NAME
 
+if (!BUCKET_NAME) {
+  throw new Error('BUCKET_NAME is not defined')
+}
+
 if (!process.env.AWS_REGION) {
   throw new Error('AWS_REGION is not defined')
 }
