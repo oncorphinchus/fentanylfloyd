109cd7ecf88f4b4b84085dcc6f9c619a9df6d1b3
oncorphinchus <tri.nguyen@bbzbl-it.ch>
2025-02-26 13:59:05 +0100

    vercel edit aws

--- app/lib/aws-config.ts
+++ app/lib/aws-config.ts
@@ -2,14 +2,22 @@ import { S3Client } from '@aws-sdk/client-s3'
 
 export const BUCKET_NAME = process.env.BUCKET_NAME
 
-if (!BUCKET_NAME) {
-  throw new Error('BUCKET_NAME is not defined')
+if (!process.env.AWS_REGION) {
+  throw new Error('AWS_REGION is not defined')
+}
+
+if (!process.env.AWS_ACCESS_KEY_ID) {
+  throw new Error('AWS_ACCESS_KEY_ID is not defined')
+}
+
+if (!process.env.AWS_SECRET_ACCESS_KEY) {
+  throw new Error('AWS_SECRET_ACCESS_KEY is not defined')
 }
 
 export const s3Client = new S3Client({
-  region: process.env.CUSTOM_AWS_REGION,
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   }
 }) 
